8351f1c2ea59ebf9a9c46f0f245ded1f58f206ce
Jason Barry <jb@netlify.com>
2023-05-22 14:16:20 -0700

    Now we're getting somewhere

--- plugins/csp-nonce/edge-function.ts
+++ plugins/csp-nonce/edge-function.ts
@@ -1,22 +1,17 @@
 import { Config, Context } from "netlify:edge";
 
 export default async (request: Request, context: Context) => {
-
-  console.log(request.headers)
-
   const response = await context.next();
-  response.headers = {
-    ...response.headers,
-    'Content-Security-Policy': "script-src 'nonce-2726c7f26c'",
-  }
+  response.headers.set(
+    "Content-Security-Policy",
+    "script-src 'nonce-2726c7f26c'"
+  );
   const page = await response.text();
 
-  console.log('page', page)
-
-  const updatedPage = page.replace(/<script>/gi, "<script nonce=\"2726c7f26c\">");
+  const updatedPage = page.replace(/<script>/gi, '<script nonce="2726c7f26c">');
   return new Response(updatedPage, response);
 };
 
 export const config: Config = {
-  path: '/',
+  path: "/",
 };
