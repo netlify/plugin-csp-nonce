3ea992155e3b99a632c3fd4d5a8411fdd1f3c38c
Jason Barry <jb@netlify.com>
2023-05-22 14:12:49 -0700

    More logging

--- plugins/csp-nonce/edge-function.ts
+++ plugins/csp-nonce/edge-function.ts
@@ -1,6 +1,8 @@
-import { Context } from "netlify:edge";
+import { Config, Context } from "netlify:edge";
 
 export default async (request: Request, context: Context) => {
+
+  console.log(request.headers)
 
   const response = await context.next();
   response.headers = {
@@ -9,6 +11,12 @@ export default async (request: Request, context: Context) => {
   }
   const page = await response.text();
 
+  console.log('page', page)
+
   const updatedPage = page.replace(/<script>/gi, "<script nonce=\"2726c7f26c\">");
   return new Response(updatedPage, response);
+};
+
+export const config: Config = {
+  path: '/',
 };
--- plugins/csp-nonce/index.js
+++ plugins/csp-nonce/index.js
@@ -2,11 +2,11 @@
 
 export const onPreBuild = async ({ netlifyConfig, utils }) => {
   // console.log(netlifyConfig)
+
   let dir = netlifyConfig.build.edge_functions
   if (!dir) {
     dir = './netlify/edge-functions'
     await utils.run.command(`mkdir -p ${dir}`)
   }
   await utils.run.command(`cp ./plugins/csp-nonce/edge-function.ts ${dir}/__csp-nonce.ts`)
-  
 }
