e312d8225dad4513858f3c333bd4233248109054
Jason Barry <jb@netlify.com>
2023-05-22 14:26:25 -0700

    Actually generate nonce

--- plugins/csp-nonce/edge-function.ts
+++ plugins/csp-nonce/edge-function.ts
@@ -1,14 +1,19 @@
+import { cryptoRandomString } from "https://deno.land/x/crypto_random_string@1.0.0/mod.ts";
 import { Config, Context } from "netlify:edge";
 
 export default async (request: Request, context: Context) => {
+  console.log(request.headers);
   const response = await context.next();
+
+  const nonce = cryptoRandomString({ length: 16, type: "alphanumeric" });
+
   response.headers.set(
     "Content-Security-Policy",
-    "script-src 'nonce-2726c7f26c'"
+    `script-src 'nonce-${nonce}'`
   );
   const page = await response.text();
 
-  const updatedPage = page.replace(/<script>/gi, '<script nonce="2726c7f26c">');
+  const updatedPage = page.replace(/<script>/gi, `<script nonce="${nonce}">`);
   return new Response(updatedPage, response);
 };
 
