8bdb9157281910b6be9ddee35164472e7037482d
Jason Barry <jb@netlify.com>
2023-05-30 11:59:15 -0700

    Update with latest

--- plugins/csp-nonce/index.js
+++ plugins/csp-nonce/index.js
@@ -1,10 +1,29 @@
-export const onPreBuild = async ({ netlifyConfig, utils }) => {
-  let dir = netlifyConfig.build.edge_functions;
-  if (!dir) {
-    dir = "./netlify/edge-functions";
-    await utils.run.command(`mkdir -p ${dir}`);
-  }
-  await utils.run.command(
-    `cp ./plugins/csp-nonce/edge-function.ts ${dir}/__csp-nonce.ts`
+import fs, { copyFileSync } from "fs";
+
+/* eslint-disable no-console */
+export const onPreBuild = async ({ inputs, netlifyConfig, utils }) => {
+  const config = JSON.stringify(inputs, null, 2);
+
+  const functionsDir = netlifyConfig.build.functions || "./netlify/functions";
+  // make the directory in case it actually doesn't exist yet
+  await utils.run.command(`mkdir -p ${functionsDir}`);
+  console.log(`  Copying function to ${functionsDir}...`);
+  copyFileSync(
+    `./plugins/csp-nonce/src/__csp-violations.ts`,
+    `${functionsDir}/__csp-violations.ts`
   );
+
+  const edgeFunctionsDir =
+    netlifyConfig.build.edge_functions || "./netlify/edge-functions";
+  // make the directory in case it actually doesn't exist yet
+  await utils.run.command(`mkdir -p ${edgeFunctionsDir}`);
+  console.log(`  Copying edge function to ${edgeFunctionsDir}...`);
+  copyFileSync(
+    `./plugins/csp-nonce/src/__csp-nonce.ts`,
+    `${edgeFunctionsDir}/__csp-nonce.ts`
+  );
+  console.log(`  Copying config inputs to ${edgeFunctionsDir}...`);
+  fs.writeFileSync(`${edgeFunctionsDir}/__csp-nonce-inputs.json`, config);
+
+  console.log(`  Complete.`);
 };
---
+++ plugins/csp-nonce/src/__csp-nonce.ts
@@ -0,0 +1,145 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+// @ts-expect-error
+import { cryptoRandomString } from "https://deno.land/x/crypto_random_string@1.0.0/mod.ts";
+// @ts-expect-error
+import type { Config, Context } from "netlify:edge";
+
+import inputs from "./__csp-nonce-inputs.json" assert { type: "json" };
+
+type Params = {
+  reportOnly: boolean;
+  path: string | string[];
+  excludedPath: string[];
+};
+const params = inputs as Params;
+
+const header = params.reportOnly
+  ? "content-security-policy-report-only"
+  : "content-security-policy";
+
+const handler = async (request: Request, context: Context) => {
+  const response = await context.next();
+
+  // for debugging which routes use this edge function
+  response.headers.set("x-debug-csp-nonce", "invoked");
+
+  // html only
+  if (
+    !request.headers.get("accept")?.startsWith("text/html") ||
+    !response.headers.get("content-type").startsWith("text/html")
+  ) {
+    return response;
+  }
+
+  const nonce = cryptoRandomString({ length: 16, type: "alphanumeric" });
+  // `'strict-dynamic'` allows scripts to be loaded from trusted scripts
+  // when `'strict-dynamic'` is present, `'unsafe-inline' 'self' https: http:` is ignored by browsers
+  // `'unsafe-inline' 'self' https: http:` is a compat check for browsers that don't support `strict-dynamic`
+  // https://content-security-policy.com/strict-dynamic/
+  const rules = `'nonce-${nonce}' 'strict-dynamic' 'unsafe-inline' 'self' https: http:`;
+  const scriptSrc = `script-src ${rules}`;
+  const styleSrc = `style-src ${rules}`;
+  const reportUri = `report-uri /.netlify/functions/__csp-violations`;
+
+  const csp = response.headers.get(header);
+  if (csp) {
+    const directives = csp
+      .split(";")
+      .map((directive) => {
+        // prepend our rules for any existing directives
+        const d = directive.trim();
+        if (d.startsWith("script-src")) {
+          return d.replace("script-src", scriptSrc);
+        }
+        if (d.startsWith("style-src")) {
+          return d.replace("style-src", styleSrc);
+        }
+        return d;
+      })
+      .filter(Boolean);
+    // push our rules if the directives don't exist yet
+    if (!directives.find((d) => d.startsWith("script-src"))) {
+      directives.push(scriptSrc);
+    }
+    if (!directives.find((d) => d.startsWith("style-src"))) {
+      directives.push(styleSrc);
+    }
+    const value = directives.join("; ");
+    response.headers.set(header, value);
+  } else {
+    // make a new ruleset of directives if no CSP present
+    const value = [scriptSrc, styleSrc, reportUri].join("; ");
+    response.headers.set(header, value);
+  }
+
+  // time to do some regex magic
+  const page = await response.text();
+  const rewrittenPage = page.replace(
+    /<(script|style)([^>]*)>/gi,
+    `<$1$2 nonce="${nonce}">`
+  );
+  return new Response(rewrittenPage, response);
+};
+
+const excludedExtensions = [
+  "aspx",
+  "avif",
+  "babylon",
+  "bak",
+  "cgi",
+  "com",
+  "css",
+  "ds",
+  "env",
+  "gif",
+  "gz",
+  "ico",
+  "ini",
+  "jpeg",
+  "jpg",
+  "js",
+  "json",
+  "jsp",
+  "log",
+  "m4a",
+  "map",
+  "md",
+  "mjs",
+  "mp3",
+  "mp4",
+  "ogg",
+  "otf",
+  "pdf",
+  "php",
+  "png",
+  "rar",
+  "sh",
+  "sql",
+  "svg",
+  "ttf",
+  "txt",
+  "wasm",
+  "wav",
+  "webm",
+  "webmanifest",
+  "webp",
+  "woff",
+  "woff2",
+  "xml",
+  "xsd",
+  "yaml",
+  "yml",
+  "zip",
+];
+
+export const config: Config = {
+  path: params.path,
+  excludedPath: [
+    ...params.excludedPath,
+    ...excludedExtensions.map((ext) => `**/*.${ext}`),
+  ],
+  handler,
+};
+
+export default handler;
