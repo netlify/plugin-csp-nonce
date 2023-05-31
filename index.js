79382589e88466496f387119c834b133986856ed
Jason Barry <jb@netlify.com>
2023-05-31 09:31:33 -0700

    chore: tweak release-please (#3)
    * chore: tweak release-please
    
    * fix: fix path
    
    * chore: print cwd
    
    * chore: rearrange files
    
    * fix: typo in filename

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import fs, { copyFileSync } from "fs";
 
 /* eslint-disable no-console */
 export const onPreBuild = async ({ inputs, netlifyConfig, utils }) => {
+  console.log(`  Current working directory: ${process.cwd()}`);
   const config = JSON.stringify(inputs, null, 2);
 
   const functionsDir = netlifyConfig.build.functions || "./netlify/functions";
@@ -9,7 +10,7 @@ export const onPreBuild = async ({ inputs, netlifyConfig, utils }) => {
   await utils.run.command(`mkdir -p ${functionsDir}`);
   console.log(`  Copying function to ${functionsDir}...`);
   copyFileSync(
-    `./plugins/csp-nonce/files/__csp-violations.ts`,
+    `./src/__csp-violations.ts`,
     `${functionsDir}/__csp-violations.ts`
   );
 
@@ -18,10 +19,7 @@ export const onPreBuild = async ({ inputs, netlifyConfig, utils }) => {
   // make the directory in case it actually doesn't exist yet
   await utils.run.command(`mkdir -p ${edgeFunctionsDir}`);
   console.log(`  Copying edge function to ${edgeFunctionsDir}...`);
-  copyFileSync(
-    `./plugins/csp-nonce/files/__csp-nonce.ts`,
-    `${edgeFunctionsDir}/__csp-nonce.ts`
-  );
+  copyFileSync(`./src/__csp-nonce.ts`, `${edgeFunctionsDir}/__csp-nonce.ts`);
   console.log(`  Copying config inputs to ${edgeFunctionsDir}...`);
   fs.writeFileSync(`${edgeFunctionsDir}/__csp-nonce-inputs.json`, config);
 
