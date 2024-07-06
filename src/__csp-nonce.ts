e22d95b2e8627f92d56a735a38bf002beb661c3a
Christoph Werner <codepunkt@users.noreply.github.com>
2024-07-06 23:38:12 +0200

    feat: add the ability to define style-src

--- src/__csp-nonce.ts
+++ src/__csp-nonce.ts
@@ -15,6 +15,7 @@ type Params = {
   unsafeEval: boolean;
   path: string | string[];
   excludedPath: string[];
+  styleSrc: boolean;
 };
 const params = inputs as Params;
 
@@ -78,7 +79,10 @@ const handler = async (request: Request, context: Context) => {
     `https:`,
     `http:`,
   ].filter(Boolean);
-  const scriptSrc = `script-src ${rules.join(" ")}`;
+
+  const joinedRules = ${rules.join(" ");
+  const scriptSrc = `script-src ${joinedRules}`;
+  const styleSrc = `style-src ${joinedRules}`;
   const reportUri = `report-uri ${
     params.reportUri || "/.netlify/functions/__csp-violations"
   }`;
@@ -96,6 +100,12 @@ const handler = async (request: Request, context: Context) => {
           // https://github.com/netlify/plugin-csp-nonce/issues/72
           return d.replace("script-src ", `${scriptSrc} `).trim();
         }
+        // intentionally add trailing space to avoid mangling `style-src-elem`
+        if (params.styleSrc && d.startsWith("style-src ")) {
+          // append with trailing space to include any user-supplied values
+          // https://github.com/netlify/plugin-csp-nonce/issues/72
+          return d.replace("style-src ", `${styleSrc} `).trim();
+        }
         // intentionally omit report-uri: theirs should take precedence
         return d;
       })
@@ -104,6 +114,9 @@ const handler = async (request: Request, context: Context) => {
     if (!directives.find((d) => d.startsWith("script-src "))) {
       directives.push(scriptSrc);
     }
+    if (params.styleSrc && !directives.find((d) => d.startsWith("style-src "))) {
+      directives.push(styleSrc);
+    }
     if (!directives.find((d) => d.startsWith("report-uri"))) {
       directives.push(reportUri);
     }
@@ -111,7 +124,7 @@ const handler = async (request: Request, context: Context) => {
     response.headers.set(header, value);
   } else {
     // make a new ruleset of directives if no CSP present
-    const value = [scriptSrc, reportUri].join("; ");
+    const value = [scriptSrc, params.styleSrc && styleSrc, reportUri].filter(Boolean).join("; ");
     response.headers.set(header, value);
   }
 
