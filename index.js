d3e2c71bbb8d539b1095aa38624a617c7478e4cf
Jake Champion <me@jakechampion.name>
2024-11-06 17:54:30 +0000

    ci: Add automated tests for the project and run them in CI (#94)
    * only apply the original csp header to the root path
    
    * allow running the build plugin locally
    
    * add tests to confirm the plugin is working as expected
    
    * refactor: always use the relative path and don't assume the package exists in the top-level of the relative node_modules directory

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-console */
-import fs, { copyFileSync } from "fs";
-import { getBuildInfo } from '@netlify/build-info/node';
+import fs, { copyFileSync } from "node:fs";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from 'node:url'
+import { getBuildInfo } from "@netlify/build-info/node";
 
-const SITE_ID = "321a7119-6008-49a8-9d2f-e20602b1b349";
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 async function projectUsesNextJS() {
   for (const framework of (await getBuildInfo()).frameworks) {
-    if (framework.id === 'next') {
+    if (framework.id === "next") {
       return true;
     }
   }
@@ -46,34 +48,30 @@ export const onPreBuild = async ({
   }
 
   console.log(`  Current working directory: ${process.cwd()}`);
-  const basePath =
-    build.environment.SITE_ID === SITE_ID
-      ? "./src"
-      : "./node_modules/@netlify/plugin-csp-nonce/src";
 
   // make the directory in case it actually doesn't exist yet
   await utils.run.command(`mkdir -p ${INTERNAL_EDGE_FUNCTIONS_SRC}`);
   console.log(
-    `  Writing nonce edge function to ${INTERNAL_EDGE_FUNCTIONS_SRC}...`,
+    `  Writing nonce edge function to ${INTERNAL_EDGE_FUNCTIONS_SRC}...`
   );
   copyFileSync(
-    `${basePath}/__csp-nonce.ts`,
-    `${INTERNAL_EDGE_FUNCTIONS_SRC}/__csp-nonce.ts`,
+    resolve(__dirname, `./src/__csp-nonce.ts`),
+    `${INTERNAL_EDGE_FUNCTIONS_SRC}/__csp-nonce.ts`
   );
 
   const usesNext = await projectUsesNextJS();
 
   // Do not invoke the CSP Edge Function for Netlify Image CDN requests.
-  inputs.excludedPath.push('/.netlify/images');
-  
+  inputs.excludedPath.push("/.netlify/images");
+
   // If using NextJS, do not invoke the CSP Edge Function for NextJS Image requests.
   if (usesNext) {
-    inputs.excludedPath.push('/_next/image');
+    inputs.excludedPath.push("/_next/image");
   }
-  
+
   fs.writeFileSync(
     `${INTERNAL_EDGE_FUNCTIONS_SRC}/__csp-nonce-inputs.json`,
-    JSON.stringify(inputs, null, 2),
+    JSON.stringify(inputs, null, 2)
   );
 
   // if no reportUri in config input, deploy function on site's behalf
@@ -81,11 +79,11 @@ export const onPreBuild = async ({
     // make the directory in case it actually doesn't exist yet
     await utils.run.command(`mkdir -p ${INTERNAL_FUNCTIONS_SRC}`);
     console.log(
-      `  Writing violations logging function to ${INTERNAL_FUNCTIONS_SRC}...`,
+      `  Writing violations logging function to ${INTERNAL_FUNCTIONS_SRC}...`
     );
     copyFileSync(
-      `${basePath}/__csp-violations.ts`,
-      `${INTERNAL_FUNCTIONS_SRC}/__csp-violations.ts`,
+      resolve(__dirname, `./src/__csp-violations.ts`),
+      `${INTERNAL_FUNCTIONS_SRC}/__csp-violations.ts`
     );
   } else {
     console.log(`  Using ${inputs.reportUri} as report-uri directive...`);
@@ -93,3 +91,5 @@ export const onPreBuild = async ({
 
   console.log(`  Done.`);
 };
+
+export const onPreDev = onPreBuild;
---
+++ tests/integration/test.test.ts
@@ -0,0 +1,145 @@
+import parseContentSecurityPolicy from "content-security-policy-parser";
+import * as cheerio from "cheerio";
+
+import { afterAll, beforeAll, expect, it, describe } from "vitest";
+import { serve } from "./helpers";
+
+let baseURL: string;
+
+let cleanup: () => Promise<void>;
+
+beforeAll(async () => {
+  [baseURL, cleanup] = await serve({
+    waitForFunctions: ["__csp-nonce"],
+  });
+});
+
+afterAll(async () => {
+  await cleanup()
+});
+
+describe("GET /", function () {
+  let response: Response;
+  beforeAll(async () => {
+    response = await fetch(new URL(`/`, baseURL), {
+      headers: {
+        "x-nf-debug-logging": "true",
+      },
+    });
+  });
+
+  it("__csp-nonce edge function was invoked", () => {
+    expect(response.headers.get("x-debug-csp-nonce")).to.eql("invoked");
+    expect(response.headers.get("x-nf-edge-functions")).to.match(
+      /\b__csp-nonce\b/
+    );
+  });
+
+  it("responds with a 200 status", () => {
+    expect(response.status).to.eql(200);
+  });
+
+  it("responds with a content-security-policy header", () => {
+    expect(response.headers.has("content-security-policy")).to.eql(true);
+  });
+
+  describe("content-security-policy header", () => {
+    let csp: Map<string, string[]>;
+    beforeAll(async () => {
+      csp = parseContentSecurityPolicy(
+        response.headers.get("content-security-policy") || ""
+      );
+    });
+
+    it("has correct img-src directive", () => {
+      expect(csp.get("img-src")).to.eql(["'self'", "blob:", "data:"]);
+    });
+    it("has correct script-src directive", () => {
+      const script = csp.get("script-src");
+      const nonce = /^'nonce-[-A-Za-z0-9+/]{32}'$/;
+      expect(script.find((value) => nonce.test(value))).to.match(nonce);
+      expect(script.includes("'strict-dynamic'")).to.eql(true);
+      expect(script.includes("'unsafe-inline'")).to.eql(true);
+      expect(script.includes("'unsafe-eval'")).to.eql(true);
+      expect(script.includes("'self'")).to.eql(true);
+      expect(script.includes("https:")).to.eql(true);
+      expect(script.includes("http:")).to.eql(true);
+      expect(
+        script.includes("'sha256-/Cb4VxgL2aVP0MVDvbP0DgEOUv+MeNQmZX4yXHkn/c0='")
+      ).to.eql(true);
+    });
+    it("has correct report-uri directive", () => {
+      expect(csp.get("report-uri")).to.eql([
+        "/.netlify/functions/__csp-violations",
+      ]);
+    });
+  });
+
+  describe("html nonces", () => {
+    let $: cheerio.CheerioAPI;
+    beforeAll(async () => {
+      $ = cheerio.load(await response.text());
+    });
+
+    it("has set the nonce attribute on the script elements", () => {
+      const csp = parseContentSecurityPolicy(
+        response.headers.get("content-security-policy") || ""
+      );
+      const scriptsrc = csp.get("script-src");
+      const nonce = scriptsrc
+        ?.find((v) => v.startsWith("'nonce-"))
+        ?.slice("'nonce-".length, -1);
+      const scripts = $("script");
+      for (const script of scripts) {
+        expect(script.attribs.nonce).to.eql(nonce);
+      }
+    });
+  });
+});
+
+describe("POST /", function () {
+  let response: Response;
+  beforeAll(async () => {
+    response = await fetch(new URL(`/`, baseURL), {
+      method: "POST",
+      headers: {
+        "x-nf-debug-logging": "true",
+      },
+    });
+  });
+
+  it("__csp-nonce edge function was not invoked", () => {
+    expect(response.headers.has("x-debug-csp-nonce")).to.eql(false);
+    expect(response.headers.has("x-nf-edge-functions")).to.eql(false);
+  });
+
+  it("responds with original content-security-policy header", () => {
+    expect(response.headers.get("content-security-policy")).to.eql(
+      "img-src 'self' blob: data:; script-src 'sha256-/Cb4VxgL2aVP0MVDvbP0DgEOUv+MeNQmZX4yXHkn/c0='"
+    );
+  });
+});
+
+describe("GET /main.css", function () {
+  let response: Response;
+  beforeAll(async () => {
+    response = await fetch(new URL(`/main.css`, baseURL), {
+      headers: {
+        "x-nf-debug-logging": "true",
+      },
+    });
+  });
+
+  it("__csp-nonce edge function was not invoked", () => {
+    expect(response.headers.has("x-debug-csp-nonce")).to.eql(false);
+    expect(response.headers.has("x-nf-edge-functions")).to.eql(false);
+  });
+
+  it("responds with a 200 status", () => {
+    expect(response.status).to.eql(200);
+  });
+
+  it("responds without a content-security-policy header", () => {
+    expect(response.headers.has("content-security-policy")).to.eql(false);
+  });
+});
