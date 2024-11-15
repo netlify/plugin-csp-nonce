5840973ff66a3e5efdbc2d34fbb5c85e944a2794
jake champion <jake.champion@netlify.com>
2024-11-15 23:42:56 +0000

    downgrade lolhtml

--- src/__csp-nonce.ts
+++ src/__csp-nonce.ts
@@ -2,7 +2,7 @@
 // @ts-ignore
 import type { Config, Context } from "netlify:edge";
 // @ts-ignore
-import { csp } from "https://deno.land/x/csp_nonce_html_transformer@v2.1.2/src/index.ts";
+import { csp } from "https://cdn.jsdelivr.net/gh/netlify/csp_nonce_html_transformer@2de23dbb8c221f7f4537efd00378b71e7af27e7f/src/index.ts";
 // @ts-ignore
 import inputs from "./__csp-nonce-inputs.json" assert { type: "json" };
 
