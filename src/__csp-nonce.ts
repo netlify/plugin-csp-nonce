e839e39fbb305deb613fdb27067625b90f9f2260
Mateusz Bocian <mrstork@users.noreply.github.com>
2025-07-09 19:07:13 -0400

    fix: replace assert syntax with eval to be forwards compatible

--- src/__csp-nonce.ts
+++ src/__csp-nonce.ts
@@ -3,8 +3,12 @@
 import type { Config, Context } from "netlify:edge";
 // @ts-ignore
 import { csp } from "https://deno.land/x/csp_nonce_html_transformer@v2.2.2/src/index-embedded-wasm.ts";
-// @ts-ignore
-import inputs from "./__csp-nonce-inputs.json" assert { type: "json" };
+
+// @ts-ignore top-level await
+const { default: inputs } = await import("./__csp-nonce-inputs.json", {
+  // @ts-ignore 'with' syntax
+  with: { type: "json" },
+});
 
 type Params = {
   reportOnly: boolean;
